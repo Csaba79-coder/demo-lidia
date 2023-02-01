6687b0775311550f8bfd05e35830452747ef74c8
Csaba79-coder <csabavadasz79@gmail.com>
2023-02-01 18:41:05 +0100

    Initial commit

---
+++ src/main/java/com/lidia/demolidia/DemoLidiaApplication.java
@@ -0,0 +1,13 @@
+package com.lidia.demolidia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLidiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoLidiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lidia/demolidia/DemoLidiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lidia.demolidia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLidiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
